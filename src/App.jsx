72da95b75f82033c60b040c48dfb92c19cedd1fd
Prateek Gupta <guptaprateek790@gmail.com>
2026-02-12 15:30:50 +0530

    Radio Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import "./App.css";
+import { RadioGroup, RadioOption } from "./Radio";
+import { useState } from "react";
 
 function App() {
-const buttonTitle = "Submit";
+  const [selected, setSelected] = useState("");
   return (
-    <>
-      <div>
-        <button class = 'button button-blue'> 
-          <span>
-            {buttonTitle}
-          </span>
-             </button>
-      </div>
-
-    </>
-  )
+    <div className="App">
+      <h2>How did you hear about Little Lemon?</h2>
+      <RadioGroup onChange={setSelected} selected={selected}>
+        <RadioOption value="social_media">Social Media</RadioOption>
+        <RadioOption value="friends">Friends</RadioOption>
+        <RadioOption value="advertising">Advertising</RadioOption>
+        <RadioOption value="other">Other</RadioOption>
+      </RadioGroup>
+      <button disabled={!selected}>Submit</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
